948a71d680e63f8d0bdb952d1b08e43fd3e6a316
Davidslc <djjohnson@gmail.com>
2015-01-05 17:10:34 -0700

    completed first part of exercise1

---
+++ Exercise1.py
@@ -0,0 +1,16 @@
+__author__ = 'davidjohnson'
+
+ans1 = int(raw_input("Please enter the first integer: "))
+
+ans2 = int(raw_input("Please enter the second number: "))
+
+print "The sum of %d and %d is: %d" % (ans1, ans2, ans1 + ans2)
+
+print "The difference of %d and %d is: %d" % (ans1, ans2, ans1 - ans2)
+
+print "The product of %d and %d is: %d" % (ans1, ans2, ans1 * ans2)
+
+print "The quotient of %d and %d is: %d with remainder: %d" % (ans1, ans2, (ans1 / ans2), (ans1 % ans2))
+
+
+
