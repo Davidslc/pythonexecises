43e1765506c5f52f1bad153540c55147d89942fd
Davidslc <djjohnson@gmail.com>
2015-01-09 07:45:49 -0700

    initial code for exercise3

--- exercise2.py
+++ exercise2.py
@@ -1,9 +1,20 @@
+# request user input for gallons
 gallons = float(raw_input("Please enter the number of gallons of gasoline: "))
 
+# assign variables product values
+liters = 3.78541
+barrels_oil = 19.5
+co2 = 20.0
+gas_ethanol = (115000.0 / 75700.0)
+dollars = 4.0
+
+# print statements that run conversions using variables and float decimal-place string formatting
 print "Original number of gallons is: %.1f" % gallons
 
-print "%.1f  gallons is the equivalent of %.2f liters" % (gallons, gallons * 3.78541)
+print "%.1f  gallons is the equivalent of %.2f liters" % (gallons, gallons * liters)
 
-print "%.1f  gallons of gasoline requires %.11f barrels of oil" % (gallons, gallons / 19.5)
+print "%.1f  gallons of gasoline requires %.2f barrels of oil" % (gallons, gallons / barrels_oil)
 
-print "%.1f  gallons of gasoline produces %.1f pounds of CO2" % (gallons, gallons * 20)
+print "%.1f  gallons of gasoline is energy equivalent to %.2f gallons of ethanol" % (gallons, gallons * gas_ethanol)
+
+print "%.1f  gallons of gasoline requires %.2f US dollars" % (gallons, gallons * dollars)
---
+++ exercise3.py
@@ -0,0 +1,29 @@
+miles_per_hour = raw_input("Please enter a speed in miles/hour: ")
+
+# to make sure the value entered is a number
+while miles_per_hour.isalpha():
+    miles_per_hour = raw_input("Please enter miles/hour using numbers, not letters: ")
+
+mph = float(miles_per_hour)
+
+# conversions
+meters_per_hour = 1609.34 * mph
+yards_per_hour = 1760.0 * mph
+barleycorn_per_hour = 117.647 * meters_per_hour
+barleycorn_per_day = 24.0 * barleycorn_per_hour
+furlong_per_hour = yards_per_hour / 220.0
+furlong_per_day = 24.0 * furlong_per_hour
+feet_per_hour = 5280.0 * mph
+mach = (feet_per_hour / 60 / 60) / 1130.0
+psl = ((meters_per_hour / 60 / 60) / 299792458.0) * 100
+
+print "Original speed in mph is: %.f" % mph
+print "Converted to barleycorn/day is: %.f " % barleycorn_per_day
+print "Converted to furlongs/fortnight is: %.f " % (furlong_per_day * 14)
+print "Converted to Mach number is : %f" % mach
+print "Converted to the percentage of the speed of light is: %g" % psl
+
+
+
+
+
