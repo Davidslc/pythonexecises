bdda3a622a35b8643db5932c9cac08653a519f1b
Davidslc <djjohnson@gmail.com>
2015-01-13 09:11:55 -0700

    update for loop and add another for loop

--- exercise5_2.py
+++ exercise5_2.py
@@ -1,17 +1,26 @@
+import string
 
-file = open('exercise5_book.txt', 'r+', encoding="utf-8-sig")
 
+file = open('exercise5_book.txt', 'r', encoding="utf-8-sig")
+
+removed_characters = file.
 
 word_count = {}
 
-for word in file.read().split():
-    if word not in word_count:
-        word_count[word] = 1
-    else:
-        word_count[word] += 1
+# print(file.read())
+for line in file:
+    line = line.split()
+
+    for word in line:
+        word = word.lower()
+        if word not in word_count:
+            word_count[word] = 1
+        else:
+            word_count[word] += 1
 
 for key, value in word_count.items():
     print("%s ==> %s" % (key, value))
 
 file.close()
 print(file.closed)
+print(string.punctuation)
