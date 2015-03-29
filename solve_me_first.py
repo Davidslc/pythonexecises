eab6a8675b960c93fe08fc0f2d3380ac1aa7db8f
Davidslc <djjohnson@gmail.com>
2015-03-29 12:16:23 -0600

    Completed solve_me_first code challenge.

---
+++ solve_me_first.py
@@ -0,0 +1,13 @@
+"""Complete the function solveMeFirst which takes two integers and returns their sum."""
+
+
+num1 = int(input('First number: '))
+num2 = int(input('Second number: '))
+
+
+def solve_me_first(a, b):
+    return a + b
+
+res = solve_me_first(num1, num2)
+print(res)
+
